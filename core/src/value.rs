dd739d07f0872b00de9c5b4db920d9f3c9aa5e64
maekawatoshiki <konndennsa@gmail.com>
2022-06-05 01:53:14 +0900

    [core] Implement ONNX model loader

--- core/src/value.rs
+++ core/src/value.rs
@@ -3,13 +3,17 @@ use id_arena::{Arena, Id};
 pub type ValueId = Id<Value>;
 
 #[derive(Debug, Clone, PartialEq, Eq, Hash)]
-pub struct Value;
+pub struct Value(pub Option<String>);
 
 #[derive(Default)]
 pub struct ValueArena(Arena<Value>);
 
 impl ValueArena {
     pub fn new_val(&mut self) -> ValueId {
-        self.0.alloc(Value)
+        self.0.alloc(Value(None))
+    }
+
+    pub fn new_val_named(&mut self, name: impl Into<String>) -> ValueId {
+        self.0.alloc(Value(Some(name.into())))
     }
 }

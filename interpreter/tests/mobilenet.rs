883dc6c3f3a502daf2530b35b50209c880159bd6
maekawatoshiki <konndennsa@gmail.com>
2022-06-28 10:55:05 +0000

    [interpreter] Add test

--- interpreter/tests/mobilenet.rs
+++ interpreter/tests/mobilenet.rs
@@ -1,21 +1,11 @@
 use altius_core::{onnx::load_onnx, tensor::Tensor};
 use altius_interpreter::Interpreter2;
 use std::cmp::Ordering;
-use std::fs;
 use std::path::Path;
-use structopt::StructOpt;
 
-#[derive(Debug, StructOpt)]
-#[structopt(name = "compile")]
-pub struct Opt {
-    #[structopt(long = "profile", help = "Enable profiling")]
-    pub profile: bool,
-}
-
-fn main() {
+#[test]
+fn mobilenet() {
     env_logger::init();
-
-    let opt = Opt::from_args();
     let root = Path::new(env!("CARGO_MANIFEST_DIR")).join("../models");
     let model = load_onnx(root.join("mobilenetv3.onnx")).unwrap();
     let input_value = model.lookup_named_value("input").unwrap();
@@ -27,18 +17,28 @@ fn main() {
         let std = [0.229, 0.224, 0.225][c];
         (resized[(x as _, y as _)][c] as f32 / 255.0 - mean) / std
     });
-    let input = Tensor::new(vec![1, 3, 224, 224].into(), image.into_raw_vec());
+    let input = Tensor::new(
+        vec![4, 3, 224, 224].into(),
+        image
+            .clone()
+            .into_raw_vec()
+            .into_iter()
+            .chain(image.clone().into_iter())
+            .chain(image.clone().into_iter())
+            .chain(image.into_iter())
+            .collect::<Vec<_>>(),
+    );
 
-    let mut i = Interpreter2::new(&model).with_profiling(opt.profile);
+    let mut i = Interpreter2::new(&model).with_profiling(true);
     #[cfg(feature = "cuda")]
     Interpreter2::new(&model).run(vec![(input_value, input.clone())]); // First run is slow so
                                                                        // ignore it.
     let out = i.run(vec![(input_value, input)]);
     let mut out = out.data::<f32>().iter().enumerate().collect::<Vec<_>>();
-    out.sort_by(|(_, a), (_, b)| b.partial_cmp(a).unwrap_or(Ordering::Equal));
+    out[0..1000].sort_by(|(_, a), (_, b)| b.partial_cmp(a).unwrap_or(Ordering::Equal));
+    out[1000..2000].sort_by(|(_, a), (_, b)| b.partial_cmp(a).unwrap_or(Ordering::Equal));
+    out[2000..3000].sort_by(|(_, a), (_, b)| b.partial_cmp(a).unwrap_or(Ordering::Equal));
+    out[3000..4000].sort_by(|(_, a), (_, b)| b.partial_cmp(a).unwrap_or(Ordering::Equal));
 
-    let classes = fs::read_to_string(Path::new(&root).join("imagenet_classes.txt")).unwrap();
-    let classes = classes.split("\n").collect::<Vec<_>>();
-    println!("inferred: {}", classes[out[0].0]);
-    println!("top5: {:?}", &out[..5]);
+    assert!(out[0].0 == 285 && out[1000].0 == 1285 && out[2000].0 == 2285 && out[3000].0 == 3285);
 }

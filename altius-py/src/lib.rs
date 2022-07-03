111bca24144644daf809dc5967a23159a087d5c7
maekawatoshiki <konndennsa@gmail.com>
2022-07-03 23:16:21 +0900

    [altius-py] Add 'load' 'session'

--- altius-py/src/lib.rs
+++ altius-py/src/lib.rs
@@ -1,14 +1,37 @@
-use pyo3::prelude::*;
+extern crate altius_core;
+extern crate altius_interpreter;
 
-/// Formats the sum of two numbers as string.
+use altius_core::model::Model;
+use altius_interpreter::Interpreter2;
+use pyo3::{exceptions::PyRuntimeError, prelude::*};
+
+#[pyclass]
+#[repr(transparent)]
+#[derive(Clone)]
+pub struct PyModel(pub Model);
+
+#[pyclass]
+#[repr(transparent)]
+pub struct PySession(pub Interpreter2<'static>);
+
 #[pyfunction]
-fn sum_as_string(a: usize, b: usize) -> PyResult<String> {
-    Ok((a + b).to_string())
+fn load(path: String) -> PyResult<PyModel> {
+    altius_core::onnx::load_onnx(path).map_or(
+        Err(PyRuntimeError::new_err("Failed to load a ONNX model")),
+        |m| Ok(PyModel(m)),
+    )
+}
+
+#[pyfunction]
+fn session<'a>(model: &'a PyModel) -> PyResult<PySession> {
+    let model = unsafe { std::mem::transmute::<&'a Model, &'static Model>(&model.0) };
+    Ok(PySession(Interpreter2::new(model)))
 }
 
 /// A Python module implemented in Rust.
 #[pymodule]
 fn altius_py(_py: Python, m: &PyModule) -> PyResult<()> {
-    m.add_function(wrap_pyfunction!(sum_as_string, m)?)?;
+    m.add_function(wrap_pyfunction!(load, m)?)?;
+    m.add_function(wrap_pyfunction!(session, m)?)?;
     Ok(())
 }

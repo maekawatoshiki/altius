5859315e141327e4a7637b0c8853f06c6a9d13db
maekawatoshiki <konndennsa@gmail.com>
2022-08-11 17:29:40 +0900

    [interpreter][altius-py] Add 'InferenceError'

--- interpreter/examples/mobilenet.rs
+++ interpreter/examples/mobilenet.rs
@@ -33,7 +33,7 @@ fn main() {
     #[cfg(feature = "cuda")]
     Interpreter::new(&model).run(vec![(input_value, input.clone())]); // First run is slow so
                                                                       // ignore it.
-    let out = i.run(vec![(input_value, input)]);
+    let out = i.run(vec![(input_value, input)]).expect("Inference failed");
     let mut out = out[0].data::<f32>().iter().enumerate().collect::<Vec<_>>();
     out.sort_by(|(_, a), (_, b)| b.partial_cmp(a).unwrap_or(Ordering::Equal));
 

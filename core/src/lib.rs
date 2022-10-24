98844a3bf30ff7f183a90ae898a034beea09a18c
maekawatoshiki <konndennsa@gmail.com>
2022-10-24 22:17:49 +0900

    [core] Do not use mimalloc for wasm32 target

--- core/src/lib.rs
+++ core/src/lib.rs
@@ -6,7 +6,9 @@ pub mod optimize;
 pub mod tensor;
 pub mod value;
 
+#[cfg(not(target_arch = "wasm32"))]
 use mimalloc::MiMalloc;
 
+#[cfg(not(target_arch = "wasm32"))]
 #[global_allocator]
 static GLOBAL: MiMalloc = MiMalloc;

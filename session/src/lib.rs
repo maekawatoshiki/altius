564a3235a68066cdbf7cc8847138c23d2db6e227
maekawatoshiki <konndennsa@gmail.com>
2022-10-26 23:53:24 +0900

    [session] Playing around with wgpu

--- session/src/lib.rs
+++ session/src/lib.rs
@@ -3,6 +3,8 @@
 pub mod interpreter;
 pub mod wgpu;
 
+use std::fmt::Display;
+
 #[cfg(all(feature = "cblas", not(feature = "blis")))]
 #[allow(unused)]
 use blas_src;
@@ -12,4 +14,14 @@ use blis_src;
 use thiserror::Error;
 
 #[derive(Debug, Clone, Error)]
-pub enum SessionError {}
+pub enum SessionError {
+    Message(String),
+}
+
+impl Display for SessionError {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        match self {
+            Self::Message(msg) => writeln!(f, "{msg}"),
+        }
+    }
+}

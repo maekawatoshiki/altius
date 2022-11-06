95a4aeba1ef603513b054b023b12317536836b77
maekawatoshiki <konndennsa@gmail.com>
2022-11-06 15:12:59 +0000

    [session] example(infer): Run inference

--- session/examples/infer.rs
+++ session/examples/infer.rs
@@ -1,7 +1,9 @@
 use altius_core::onnx::load_onnx;
 use altius_core::optimize::gelu_fusion::fuse_gelu;
+use altius_core::tensor::Tensor;
 use altius_session::interpreter::Interpreter;
 use std::path::PathBuf;
+use std::process::exit;
 use std::time::Instant;
 use structopt::StructOpt;
 
@@ -45,6 +47,46 @@ fn main() {
         .with_intra_op_num_threads(opt.threads);
     log::info!("create session: finished in {:?}", start.elapsed());
 
-    log::info!("todo! actual inference execution");
-    // sess.run(inputs)
+    let mut inputs = vec![];
+    for (i, &input_id) in model.inputs.iter().enumerate() {
+        if model.inits.contains_key(&input_id) {
+            continue;
+        }
+
+        let input = &model.values.inner()[input_id];
+        let name = input.name.as_ref().map(String::as_str).unwrap_or("");
+        let Some(shape) = input.shape.as_ref() else {
+            log::info!("failed to feed input({i}, name={name}): unknown shape");
+            exit(1);
+        };
+
+        log::info!(
+            "feed input({i}, name={}, ty={:?}, shape={:?}): random input",
+            name,
+            shape.elem_ty,
+            shape.dims
+        );
+
+        inputs.push((
+            input_id,
+            Tensor::rand_of_type(shape.elem_ty, shape.dims.clone()),
+        ));
+    }
+
+    let outputs = match sess.run(inputs) {
+        Ok(outputs) => outputs,
+        Err(e) => {
+            log::info!("inference failed: {:?}", e);
+            exit(1);
+        }
+    };
+
+    for (i, (output, output_id)) in outputs.iter().zip(model.outputs.iter()).enumerate() {
+        let name = model.values.inner()[*output_id]
+            .name
+            .as_ref()
+            .map(String::as_str)
+            .unwrap_or("");
+        log::info!("output({i}, name={}, shape={:?}): ...", name, output.dims());
+    }
 }

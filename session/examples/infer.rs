5ea213d7723b649878ef289f0836a02c2a07c96a
maekawatoshiki <konndennsa@gmail.com>
2022-11-06 12:20:24 +0000

    [session] example(infer): Create interpreter session

--- session/examples/infer.rs
+++ session/examples/infer.rs
@@ -1,5 +1,6 @@
 use altius_core::onnx::load_onnx;
 use altius_core::optimize::gelu_fusion::fuse_gelu;
+use altius_session::interpreter::Interpreter;
 use std::path::PathBuf;
 use std::time::Instant;
 use structopt::StructOpt;
@@ -12,6 +13,13 @@ pub struct Opt {
 
     #[structopt(long = "profile", help = "Enable profiling")]
     pub profile: bool,
+
+    #[structopt(
+        long = "threads",
+        help = "The number of threads for computation",
+        default_value = "1"
+    )]
+    pub threads: usize,
 }
 
 fn main() {
@@ -19,12 +27,24 @@ fn main() {
 
     let opt = Opt::from_args();
 
-    log::info!("load onnx: path = {:?}", opt.onnx_path);
+    log::info!("load onnx: start ({:?})", opt.onnx_path);
     let start = Instant::now();
     let mut model = load_onnx(opt.onnx_path).unwrap();
     log::info!("load onnx: finished in {:?}", start.elapsed());
 
     fuse_gelu(&mut model);
 
-    log::info!("todo!")
+    log::info!(
+        "create session: start (profile={:?}, threads={})",
+        opt.profile,
+        opt.threads
+    );
+    let start = Instant::now();
+    let sess = Interpreter::new(&model)
+        .with_profiling(opt.profile)
+        .with_intra_op_num_threads(opt.threads);
+    log::info!("create session: finished in {:?}", start.elapsed());
+
+    log::info!("todo! actual inference execution");
+    // sess.run(inputs)
 }

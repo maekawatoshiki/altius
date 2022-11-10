3810cfc62db0fd2d842d4ecd82f8e1ff2c6f1a9d
maekawatoshiki <konndennsa@gmail.com>
2022-11-10 16:21:03 +0000

    Update download.sh

--- models/download.sh
+++ models/download.sh
@@ -25,3 +25,5 @@ download 1K6m7WxoE3noMTbNPyhyxLLNlXsdN1Sq6 ./vit_b_16.onnx
 download 1E2RxWfxufLNB_cXm30RdjXt4YVbFzQrs ./bert.onnx
 download 1YP8wJyOhR0vSaeasn-z1WbkXBX8TzUER ./realesrgan_256x256.onnx
 download 1GgqkMVXL0T2ssZ-JSrMhCCJvAmpIgk8Q ./gpt2.onnx
+download 1BiXmAGt_SZdZ1OuSv3ntT6Ad5h_q7oJ4 ./mnist-8.onnx
+download 1cZtpzvERn-QXDjfbPYY_cu3RlxxQJOVP ./mobilenetv3.onnx

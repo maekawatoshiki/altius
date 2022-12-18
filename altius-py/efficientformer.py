8f162a43d340bf26a927e015f8d307e58339fcc5
maekawatoshiki <konndennsa@gmail.com>
2022-12-18 23:04:41 +0900

    [altius-py] Add example 'efficientformer.py'

---
+++ altius-py/efficientformer.py
@@ -0,0 +1,51 @@
+import time
+import logging
+import os
+
+import onnxsim
+import altius_py
+import onnx
+import torch
+from transformers import ViTImageProcessor, ViTForImageClassification
+
+import numpy as np
+from PIL import Image
+
+
+def main():
+    logging.basicConfig(level=logging.INFO)
+
+    image = Image.open("../models/cat.png")
+    labels = open("../models/imagenet_classes.txt").readlines()
+
+    feature_extractor = ViTImageProcessor.from_pretrained(
+        "facebook/deit-small-patch16-224"
+    )
+    inputs = feature_extractor(image, return_tensors="pt")
+
+    onnx_path = "../models/deit.onnx"
+
+    if not os.path.exists(onnx_path):
+        model = ViTForImageClassification.from_pretrained(
+            "facebook/deit-small-patch16-224"
+        ).to("cpu")
+        torch.onnx.export(model, torch.randn(1, 3, 224, 224), onnx_path)
+        simplified_model, success = onnxsim.simplify(onnx_path)
+        assert success
+        onnx.save(simplified_model, onnx_path)
+
+    altius_model = altius_py.InferenceSession(
+        onnx_path, intra_op_num_threads=8, enable_profile=True
+    )
+    input = inputs["pixel_values"].detach().numpy().reshape((1, 3, 224, 224))
+
+    with torch.no_grad():
+        for i in range(10):
+            output = altius_model.run(None, {"input.1": input})
+            pred = torch.tensor(output).reshape((-1,)).argsort().numpy()[::-1][:5]
+            top5 = [labels[i].strip() for i in pred]
+            print(top5)
+
+
+if __name__ == "__main__":
+    main()

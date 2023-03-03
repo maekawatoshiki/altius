3cbf147edb9e687631577eb18e43643e4f17c6cc
maekawatoshiki <konndennsa@gmail.com>
2023-03-03 20:57:33 +0900

    Fix coverage measurement in CI

--- altius-py/test.sh
+++ altius-py/test.sh
@@ -13,10 +13,10 @@ fi
 export PROTOCOL_BUFFERS_PYTHON_IMPLEMENTATION=python
 
 if [ ${1:-nobuild} = "build" ]; then
-  if [ -v $GITHUB_ACTIONS ]; then
-    maturin develop --target-dir ./target --features blis
-  else
+  if [ -v GITHUB_ACTIONS ]; then
     maturin develop --features blis
+  else
+    maturin develop --target-dir ./target --features blis
   fi
 fi
 

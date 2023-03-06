653d8c3f85006033b15074f23cd579b8bb8ca7c4
maekawatoshiki <konndennsa@gmail.com>
2023-03-06 19:33:54 +0900

    [snippets] Add float.c

---
+++ snippets/float.c
@@ -0,0 +1,51 @@
+#include <stdio.h>
+#include <assert.h>
+
+typedef struct {
+  unsigned int frac : 23;
+  unsigned int exp  : 8;
+  unsigned int sign : 1;
+} f32;
+
+float add(float x, float y) {
+  const f32 xi = *(f32*)&x;
+  const f32 yi = *(f32*)&y;
+  const f32 zi = {
+    .sign = 0,
+    .exp  = 127,
+    .frac = xi.frac + (yi.frac >> 1) + 0x400000,
+  };
+  return *(float*)&zi;
+
+#if 0
+  const int xi = *(int*)&x;
+  const int yi = *(int*)&y;
+  const int xi_sign = (xi >> 31) & 0x1;
+  const int xi_exp = (xi >> 23) & 0x7f;
+  const int xi_frac = xi & 0x7fffff;
+  const int yi_sign = (yi >> 31) & 0x1;
+  const int yi_exp = (yi >> 23) & 0x7f;
+  const int yi_frac = yi & 0x7fffff;
+  assert(xi_sign == 0);
+  assert(yi_sign == 0);
+  assert(xi_exp == 127);
+  assert(yi_exp == 127 - 1);
+  /* assert(xi_frac == 0); */
+  /* assert(yi_frac == 0); */
+  const int zi_sign = 0;
+  const int zi_exp = 127;
+  const int zi_frac = xi_frac + (yi_frac >> 1) + 0x400000;
+  const int zi = (zi_sign << 31) | (zi_exp << 23) | zi_frac;
+  return *(float*)&zi;
+#endif
+}
+
+int main() {
+  /* 1 + 8 + 7 */
+  /* sign exp frac */
+  float x = 1.0;
+  float y = 0.52;
+  float z = add(x, y);
+  printf("%f\n", z);
+  return 0;
+}

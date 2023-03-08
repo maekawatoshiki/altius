7b12493a74c889007b60b2871404625f1ac04182
maekawatoshiki <konndennsa@gmail.com>
2023-03-08 18:49:29 +0900

    [core] Disable 'matmul_add_fusion.rs' for now

--- core/src/optimize/mod.rs
+++ core/src/optimize/mod.rs
@@ -1,4 +1,4 @@
 pub mod gelu_fusion;
 pub mod layer_norm_fusion;
-pub mod matmul_add_fusion;
+// pub mod matmul_add_fusion; 
 // pub mod transpose_fusion; // TODO: Implemented but I figured out it's not useful for now.

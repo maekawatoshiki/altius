8773672eb4fecc8a82ca0fcfbb465fd445c6e8e3
maekawatoshiki <konndennsa@gmail.com>
2023-03-22 00:01:46 +0900

    [altius-py] Small fix

--- altius-py/test.sh
+++ altius-py/test.sh
@@ -13,10 +13,10 @@ fi
 export PROTOCOL_BUFFERS_PYTHON_IMPLEMENTATION=python
 
 if [ ${1:-nobuild} = "build" ]; then
-  if [ -v GITHUB_ACTIONS ]; then
-    maturin develop -r --features blis
-  else
+  if [ -z "${GITHUB_ACTIONS}" ]; then
     maturin develop -r --target-dir ./target --features blis
+  else
+    maturin develop -r --features blis
   fi
 fi
 

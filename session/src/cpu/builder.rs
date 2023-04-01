72d20d7c5c40c1b9da79ca2e5b7e006064c080fa
maekawatoshiki <konndennsa@gmail.com>
2023-04-01 23:06:45 +0900

    [session] cpu: Add 'Tanh' kernel

--- session/src/cpu/builder.rs
+++ session/src/cpu/builder.rs
@@ -539,6 +539,7 @@ static struct timespec now() {{
             Op::Sqrt => self.translate_sqrt(&args, &inputs, &outputs)?,
             Op::ReLU => self.translate_relu(&args, &inputs, &outputs)?,
             Op::Erf => self.translate_erf(&args, &inputs, &outputs)?,
+            Op::Tanh => self.translate_tanh(&args, &inputs, &outputs)?,
             Op::GlobalAveragePool => self.translate_gavg_pool(&args, &inputs, &outputs)?,
             Op::MaxPool(ref m) => self.translate_max_pool(m, &args, &inputs, &outputs)?,
             Op::Reshape => self.translate_reshape(&args, &inputs, &outputs)?,
@@ -1054,6 +1055,26 @@ for (int i = 0; i < {size}; i++) {{
 for (int i = 0; i < {size}; i++) {{
     const float x = {input_name}[i];
     {output_name}[i] = erff(x);
+}}"
+        );
+        Ok(kernel)
+    }
+
+    fn translate_tanh(
+        &mut self,
+        args: &[String],
+        inputs: &[&TypedShape],
+        _outputs: &[TypedShape],
+    ) -> Result<String, SessionError> {
+        let input_name = &args[..inputs.len()][0];
+        let output_name = &args[inputs.len()..][0];
+        let size = inputs[0].dims.total_elems();
+        let num_threads = self.intra_op_num_threads;
+        let kernel = format!(
+            "#pragma omp parallel for num_threads({num_threads})
+for (int i = 0; i < {size}; i++) {{
+    const float x = {input_name}[i];
+    {output_name}[i] = tanhf(x);
 }}"
         );
         Ok(kernel)

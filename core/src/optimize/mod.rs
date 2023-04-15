5b5fe86585ae24270e474fb6e62734dcaa69c0ab
maekawatoshiki <konndennsa@gmail.com>
2023-04-16 00:41:10 +0900

    [core] Remove 'matmul_add_fusion.rs' because it could be done by onnx simplifier

--- core/src/optimize/mod.rs
+++ core/src/optimize/mod.rs
@@ -1,5 +1,4 @@
 pub mod conv_act_fusion;
 pub mod gelu_fusion;
 pub mod layer_norm_fusion;
-// pub mod matmul_add_fusion;
 // pub mod transpose_fusion; // TODO: Implemented but I figured out it's not useful for now.

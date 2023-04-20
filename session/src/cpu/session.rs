02ef99e4ed93d77e8b4503a85f4b30608b51ec8c
maekawatoshiki <konndennsa@gmail.com>
2023-04-21 00:03:39 +0900

    [session] cpu: Implement a simple trampoline for passing arbitrary number of arguments

--- session/src/cpu/session.rs
+++ session/src/cpu/session.rs
@@ -43,41 +43,121 @@ impl CPUSession {
             .collect::<Vec<_>>();
 
         let start = Instant::now();
-        unsafe {
-            // TODO: Use macro or something better.
-            match (inputs.len(), outputs.len()) {
-                (1, 1) => {
-                    let entry = std::mem::transmute::<_, unsafe extern "C" fn(*const u8, *mut u8)>(
-                        self.entry,
-                    );
-                    entry(inputs[0].1.data_as_ptr(), outputs[0].data_as_mut_ptr());
-                }
-                (2, 1) => {
-                    let entry = std::mem::transmute::<
-                        _,
-                        unsafe extern "C" fn(*const u8, *const u8, *mut u8),
-                    >(self.entry);
-                    entry(
-                        inputs[0].1.data_as_ptr(),
-                        inputs[1].1.data_as_ptr(),
-                        outputs[0].data_as_mut_ptr(),
-                    );
-                }
-                (3, 1) => {
-                    let entry = std::mem::transmute::<
-                        _,
-                        unsafe extern "C" fn(*const u8, *const u8, *const u8, *mut u8),
-                    >(self.entry);
-                    entry(
-                        inputs[0].1.data_as_ptr(),
-                        inputs[1].1.data_as_ptr(),
-                        inputs[2].1.data_as_ptr(),
-                        outputs[0].data_as_mut_ptr(),
-                    );
-                }
-                _ => unreachable!(),
+
+        #[cfg(target_arch = "x86_64")]
+        if true {
+            use dynasm::dynasm;
+            use dynasmrt::{aarch64::Assembler, DynasmApi};
+
+            let mut ops = Assembler::new().unwrap();
+
+            let trampoline = ops.offset();
+            dynasm!(ops
+                ; .arch x64
+                ; push rbp
+                ; push r12
+                ; push r13
+                ; mov r12, rdi
+                ; mov r13, rsi
+            );
+            let param_regs = vec![7, 6, 2, 1, 8, 9]; // rdi, rsi, rdx, rcx, r8, r9
+            for i in 0..inputs.len() as i32 {
+                dynasm!(ops; mov Rq(param_regs[i as usize]), QWORD [r12+8*i]);
             }
-        };
+            for i in 0..outputs.len() as i32 {
+                dynasm!(ops; mov Rq(param_regs[inputs.len() + i as usize]), QWORD [r13+8*i]);
+            }
+            dynasm!(ops
+                ; .arch x64
+                ; mov rax, QWORD self.entry as _
+                ; call rax
+                ; pop r13
+                ; pop r12
+                ; pop rbp
+                ; ret
+            );
+            let buf = ops.finalize().unwrap();
+            let trampoline: extern "C" fn(*const *const u8, *const *mut u8) =
+                unsafe { std::mem::transmute(buf.ptr(trampoline)) };
+
+            let inputs = inputs
+                .iter()
+                .map(|(_, tensor)| tensor.data_as_ptr())
+                .collect::<Vec<_>>();
+            let outputs = outputs
+                .iter_mut()
+                .map(|tensor| tensor.data_as_mut_ptr())
+                .collect::<Vec<_>>();
+
+            trampoline(inputs.as_ptr(), outputs.as_ptr());
+        }
+
+        #[cfg(target_arch = "aarch64")]
+        {
+            unsafe {
+                // TODO: Use macro or something better.
+                match (inputs.len(), outputs.len()) {
+                    (1, 1) => {
+                        let entry = std::mem::transmute::<
+                            _,
+                            unsafe extern "C" fn(*const u8, *mut u8),
+                        >(self.entry);
+                        entry(inputs[0].1.data_as_ptr(), outputs[0].data_as_mut_ptr());
+                    }
+                    (2, 1) => {
+                        let entry = std::mem::transmute::<
+                            _,
+                            unsafe extern "C" fn(*const u8, *const u8, *mut u8),
+                        >(self.entry);
+                        entry(
+                            inputs[0].1.data_as_ptr(),
+                            inputs[1].1.data_as_ptr(),
+                            outputs[0].data_as_mut_ptr(),
+                        );
+                    }
+                    (3, 1) => {
+                        let entry = std::mem::transmute::<
+                            _,
+                            unsafe extern "C" fn(*const u8, *const u8, *const u8, *mut u8),
+                        >(self.entry);
+                        entry(
+                            inputs[0].1.data_as_ptr(),
+                            inputs[1].1.data_as_ptr(),
+                            inputs[2].1.data_as_ptr(),
+                            outputs[0].data_as_mut_ptr(),
+                        );
+                    }
+                    _ => unreachable!(),
+                }
+            };
+
+            // use dynasm::dynasm;
+            // use dynasmrt::{aarch64::Assembler, DynasmApi};
+            //
+            // let mut ops = Assembler::new().unwrap();
+            //
+            // let trampoline = ops.offset();
+            //
+            // assert_eq!(inputs.len(), 1);
+            // assert_eq!(outputs.len(), 1);
+            //
+            // dynasm!(ops
+            //     ; .arch aarch64
+            //     ; ldr x4, [x0, 0]
+            //     ; ldr x4, [x4]
+            //     // ; mov x1, x1
+            //     ; ldr x5, [x2, 0]
+            //     ; ldr x5, [x5]
+            //     // ; mov x3, x3
+            //     ; bl (self.entry as u64)
+            //     ; ret
+            // );
+            // // #[cfg(feature = "x64-fusion")]
+            // let buf = ops.finalize().unwrap();
+            // // #[cfg(feature = "x64-fusion")]
+            // let trampoline: extern "C" fn(*const *const f32, u64, *const *mut f32, u64) =
+            //     unsafe { std::mem::transmute(buf.ptr(trampoline)) };
+        }
 
         if self.enable_profiling {
             let entire_duration = start.elapsed().as_secs_f32() * 1000.0;

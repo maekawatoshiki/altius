d34999606a4755dcca3fa470a26afcbb7bca0694
maekawatoshiki <konndennsa@gmail.com>
2023-04-22 01:27:27 +0900

    [snippets] Edit cuda-gemm-act.cu

--- snippets/cuda/cuda-gemm-act.cu
+++ snippets/cuda/cuda-gemm-act.cu
@@ -4,6 +4,7 @@
 #include <cublas.h>
 #include <cublas_v2.h>
 #include <cudnn.h>
+#include <curand.h>
 
 double now_in_sec() {
   struct timeval tv;
@@ -11,11 +12,23 @@ double now_in_sec() {
   return (double)tv.tv_sec + (double)tv.tv_usec / 1000.f / 1000.f;
 }
 
+__global__ void relu(float *x, int n) {
+  const int i = blockIdx.x * blockDim.x + threadIdx.x;
+  if (i < n) {
+    x[i] = x[i] > 0.f ? x[i] : 0.f;
+  }
+}
+
+__global__ void sigmoid(float *x, int n) {
+  const int i = blockIdx.x * blockDim.x + threadIdx.x;
+  if (i < n) {
+    x[i] = 1.f / (1.f + expf(-x[i]));
+  }
+}
+
 extern "C" void entry() {
   cublasHandle_t handle;
   cublasCreate(&handle);
-
-  const double start = now_in_sec();
 
   float *lhs, *rhs, *result;
   const int m = 1000, k = 200, n = 100;
@@ -25,14 +38,27 @@ extern "C" void entry() {
   cudaMalloc(&rhs, k * n * sizeof(float));
   cudaMalloc(&result, m * n * sizeof(float));
 
-  for (int i = 0; i < 1000; i++) {
-    cublasSgemm(handle, CUBLAS_OP_N, CUBLAS_OP_N, m, n, k, &alpha, lhs, m, rhs,
-                k, &beta, result, m);
+  {
+    // fill lhs and rhs with random numbers
+    curandGenerator_t gen;
+    curandCreateGenerator(&gen, CURAND_RNG_PSEUDO_DEFAULT);
+    curandSetPseudoRandomGeneratorSeed(gen, 1234ULL);
+    curandGenerateUniform(gen, lhs, m * k);
+    curandGenerateUniform(gen, rhs, k * n);
+    curandDestroyGenerator(gen);
   }
 
-  const double end = now_in_sec();
-
-  printf("Time: %lf[ms]\n", (end - start) * 1000.0);
+  for (int attempt = 0; attempt < 100; attempt++) {
+    const double start = now_in_sec();
+    for (int i = 0; i < 1000; i++) {
+      cublasSgemm(handle, CUBLAS_OP_N, CUBLAS_OP_N, m, n, k, &alpha, lhs, m,
+                  rhs, k, &beta, result, m);
+      relu<<<(m * n + 31) / 32, 32>>>(result, m * n);
+      sigmoid<<<(m * n + 31) / 32, 32>>>(result, m * n);
+    }
+    const double end = now_in_sec();
+    printf("Time: %lf[ms]\n", (end - start) * 1000.0);
+  }
 
   float *host = (float *)malloc(m * n * sizeof(float));
   cudaMemcpy(host, result, m * n * sizeof(float), cudaMemcpyDeviceToHost);

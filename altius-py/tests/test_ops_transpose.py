16cbd9f825ce11ee11547d547d93f697d672f485
maekawatoshiki <maekawatoshiki1017@gmail.com>
2023-07-10 17:41:32 +0900

    [altius-py] Add another test for 'Transpose'

--- altius-py/tests/test_ops_transpose.py
+++ altius-py/tests/test_ops_transpose.py
@@ -10,7 +10,7 @@ from onnx import helper, ValueInfoProto, TensorProto
 
 def test_transpose_1():
     with tempfile.TemporaryDirectory() as tmpdir:
-        op_concat(
+        op_transpose(
             os.path.join(tmpdir, "model.onnx"),
             [50, 12, 64],
             [12, 64, 50],
@@ -20,7 +20,7 @@ def test_transpose_1():
 
 def test_transpose_2():
     with tempfile.TemporaryDirectory() as tmpdir:
-        op_concat(
+        op_transpose(
             os.path.join(tmpdir, "model.onnx"),
             [12, 64],
             [64, 12],
@@ -30,7 +30,7 @@ def test_transpose_2():
 
 def test_transpose_3():
     with tempfile.TemporaryDirectory() as tmpdir:
-        op_concat(
+        op_transpose(
             os.path.join(tmpdir, "model.onnx"),
             [12, 64, 3, 5],
             [3, 64, 12, 5],
@@ -38,7 +38,17 @@ def test_transpose_3():
         )
 
 
-def op_concat(filepath, shape_x, shape_y, **kwargs):
+def test_transpose_4():
+    with tempfile.TemporaryDirectory() as tmpdir:
+        op_transpose(
+            os.path.join(tmpdir, "model.onnx"),
+            [50, 12],
+            [50, 12],
+            perm=[0, 1],
+        )
+
+
+def op_transpose(filepath, shape_x, shape_y, **kwargs):
     inputs = [helper.make_tensor_value_info("x", TensorProto.FLOAT, shape_x)]
     outputs = [helper.make_tensor_value_info("y", TensorProto.FLOAT, shape_y)]
     nodes = [helper.make_node("Transpose", ["x"], ["y"], **kwargs)]
@@ -48,13 +58,15 @@ def op_concat(filepath, shape_x, shape_y, **kwargs):
     onnx.checker.check_model(model)
     onnx.save(model, filepath)
     ort_sess = ort.InferenceSession(filepath, providers=["CPUExecutionProvider"])
-    altius_sess = altius_py.InferenceSession(filepath)
 
-    x = np.random.random_sample(shape_x).astype(np.float32)
-    y = np.random.random_sample(shape_y).astype(np.float32)
-    inputs = {"x": x}
-    expected = ort_sess.run(None, inputs)
-    actual = altius_sess.run(None, inputs)
+    for backend in ["interpreter", "cpu"]:
+        altius_sess = altius_py.InferenceSession(filepath, backend=backend)
 
-    for expected, actual in zip(expected, actual):
-        assert np.allclose(expected, actual)
+        x = np.random.random_sample(shape_x).astype(np.float32)
+        y = np.random.random_sample(shape_y).astype(np.float32)
+        inputs = {"x": x}
+        expected = ort_sess.run(None, inputs)
+        actual = altius_sess.run(None, inputs)
+
+        for expected, actual in zip(expected, actual):
+            assert np.allclose(expected, actual)

4d7b90ceda560fe48ac431c122641ceabaa716b2
maekawatoshiki <maekawatoshiki1017@gmail.com>
2024-05-05 21:37:19 +0900

    [core] Refactor gelu_fusion.rs

--- crates/core/src/optimize/gelu_fusion.rs
+++ crates/core/src/optimize/gelu_fusion.rs
@@ -1,115 +1,131 @@
 use std::time::Instant;
 
-use crate::{model::Model, node::Node, op::Op};
+use rustc_hash::{FxHashMap, FxHashSet};
+
+use crate::{
+    model::Model,
+    node::{Node, NodeId},
+    op::Op,
+    value::ValueId,
+};
+
+fn extract_gelu(
+    model: &Model,
+    value_users: &FxHashMap<ValueId, FxHashSet<NodeId>>,
+    root_id: NodeId,
+) -> Option<(ValueId, ValueId, Vec<NodeId>)> {
+    let div = &model.graph.nodes[root_id];
+    let approx_sqrt_two = 1.4142099618911743f32;
+    if div.op != Op::Div
+        || model
+            .graph
+            .inits
+            .get(&div.inputs[1])
+            .map_or(true, |rhs| !rhs.allclose(&[approx_sqrt_two]))
+    {
+        return None;
+    }
+
+    let erf_id = value_users[&div.outputs[0]].iter().next().copied()?;
+    let erf = &model.graph.nodes[erf_id];
+    if erf.op != Op::Erf {
+        return None;
+    }
+
+    let add_id = value_users[&erf.outputs[0]].iter().next().copied()?;
+    let add = &model.graph.nodes[add_id];
+    if add.op != Op::Add {
+        return None;
+    }
+    let is_erf_add_lhs = add.inputs[0] == erf.outputs[0];
+    if model
+        .graph
+        .inits
+        .get(&add.inputs[is_erf_add_lhs as usize])
+        .map_or(true, |one| {
+            !one.elem_ty().is_f32() || one.data::<f32>()[0] != 1.
+        })
+    {
+        return None;
+    }
+
+    let mul1_id = value_users[&add.outputs[0]].iter().next().copied()?;
+    let mul1 = &model.graph.nodes[mul1_id];
+    if mul1.op != Op::Mul {
+        return None;
+    }
+    let is_add_mul1_lhs = mul1.inputs[0] == add.outputs[0];
+    if mul1.inputs[is_add_mul1_lhs as usize] != div.inputs[0] {
+        return None;
+    }
+
+    let mul2_id = value_users[&mul1.outputs[0]].iter().next().copied()?;
+    let mul2 = &model.graph.nodes[mul2_id];
+    if mul2.op != Op::Mul {
+        return None;
+    }
+    let is_mul1_mul2_lhs = mul2.inputs[0] == mul1.outputs[0];
+    if model
+        .graph
+        .inits
+        .get(&mul2.inputs[is_mul1_mul2_lhs as usize])
+        .map_or(true, |half| {
+            !half.elem_ty().is_f32() || half.data::<f32>()[0] != 0.5
+        })
+    {
+        return None;
+    }
+
+    // Gelu Detected!
+
+    Some((
+        div.inputs[0],
+        mul2.outputs[0],
+        vec![root_id, erf_id, add_id, mul1_id, mul2_id],
+    ))
+}
 
 pub fn fuse_gelu(model: &mut Model) {
     let start = Instant::now();
     let nodes = model.topo_sort_nodes();
     let value_users = model.get_value_users();
 
-    let mut list = vec![];
-    let mut delete_list = vec![];
+    let mut subgraphs = vec![];
+    let mut unnecessary_nodes = vec![];
 
     for node_id in nodes {
-        let div_id = node_id;
-        let div = &model.graph.nodes[div_id];
-        if !matches!(div.op, Op::Div) {
-            continue;
+        if let Some((start, end, nodes)) = extract_gelu(model, &value_users, node_id) {
+            subgraphs.push((start, end));
+            unnecessary_nodes.extend(nodes);
         }
-        let approx_sqrt_two = 1.4142099618911743f32;
-        if model
-            .graph
-            .inits
-            .get(&div.inputs[1])
-            .map_or(true, |rhs| !rhs.allclose(&[approx_sqrt_two]))
-        {
-            continue;
-        }
-
-        let erf_id = value_users[&div.outputs[0]].iter().next().copied().unwrap();
-        let erf = &model.graph.nodes[erf_id];
-        if !matches!(erf.op, Op::Erf) {
-            continue;
-        }
-
-        let add_id = value_users[&erf.outputs[0]].iter().next().copied().unwrap();
-        let add = &model.graph.nodes[add_id];
-        if !matches!(add.op, Op::Add) {
-            continue;
-        }
-        let is_erf_add_lhs = add.inputs[0] == erf.outputs[0];
-        if model
-            .graph
-            .inits
-            .get(&add.inputs[is_erf_add_lhs as usize])
-            .map_or(true, |one| {
-                !one.elem_ty().is_f32() || one.data::<f32>()[0] != 1.
-            })
-        {
-            continue;
-        }
-
-        let mul1_id = value_users[&add.outputs[0]].iter().next().copied().unwrap();
-        let mul1 = &model.graph.nodes[mul1_id];
-        if !matches!(mul1.op, Op::Mul) {
-            continue;
-        }
-        let is_add_mul1_lhs = mul1.inputs[0] == add.outputs[0];
-        if mul1.inputs[is_add_mul1_lhs as usize] != div.inputs[0] {
-            continue;
-        }
-
-        let mul2_id = value_users[&mul1.outputs[0]]
-            .iter()
-            .next()
-            .copied()
-            .unwrap();
-        let mul2 = &model.graph.nodes[mul2_id];
-        if !matches!(mul2.op, Op::Mul) {
-            continue;
-        }
-        let is_mul1_mul2_lhs = mul2.inputs[0] == mul1.outputs[0];
-        if model
-            .graph
-            .inits
-            .get(&mul2.inputs[is_mul1_mul2_lhs as usize])
-            .map_or(true, |half| {
-                !half.elem_ty().is_f32() || half.data::<f32>()[0] != 0.5
-            })
-        {
-            continue;
-        }
-
-        // Gelu Detected!
-
-        list.push((div.inputs[0], mul2.outputs[0]));
-        delete_list.push(div_id);
-        delete_list.push(erf_id);
-        delete_list.push(add_id);
-        delete_list.push(mul1_id);
-        delete_list.push(mul2_id);
     }
 
-    let count = list.len();
+    let count = subgraphs.len();
 
-    for (start, end) in list {
+    for (start, end) in subgraphs {
         let gelu_out = model.graph.values.new_val();
         let gelu = Node::new(Op::Gelu).with_in(start).with_out(gelu_out);
-        let _gelu_id = model.graph.add_node(gelu);
+        model.graph.add_node(gelu);
 
         let Some(users) = value_users.get(&end) else {
-            let idx = model.graph.outputs.iter().position(|&i| i == end).unwrap();
-            model.graph.outputs[idx] = gelu_out;
+            for output in &mut model.graph.outputs {
+                if *output == end {
+                    *output = gelu_out;
+                }
+            }
             continue;
         };
-        for user_id in users {
-            let user = &mut model.graph.nodes[*user_id];
-            let idx = user.inputs.iter().position(|&i| i == end).unwrap();
-            user.inputs[idx] = gelu_out
+        for &user_id in users {
+            let user = &mut model.graph.nodes[user_id];
+            for input in &mut user.inputs {
+                if *input == end {
+                    *input = gelu_out;
+                }
+            }
         }
     }
 
-    for node in delete_list {
+    for node in unnecessary_nodes {
         model.graph.nodes[node].deleted = true
     }
 

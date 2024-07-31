8225257dda29cdc9c679b24c8400e370625c1e99
maekawatoshiki <maekawatoshiki1017@gmail.com>
2024-07-31 13:52:28 +0900

    [altius-py] Prepare to test on multiple backends

--- crates/altius-py/tests/test_ops_elemwise.py
+++ crates/altius-py/tests/test_ops_elemwise.py
@@ -12,116 +12,143 @@ from onnx import helper, ValueInfoProto, TensorProto
 from onnxscript import FLOAT, script, opset12 as op
 
 
-def test_identity_1():
+@pytest.mark.parametrize("backend", ["interpreter"])
+def test_identity_1(backend):
     with tempfile.TemporaryDirectory() as tmpdir:
-        op_elemwise(os.path.join(tmpdir, "model.onnx"), "Identity", [1, 2])
+        op_elemwise(os.path.join(tmpdir, "model.onnx"), "Identity", [1, 2], backend)
 
 
-def test_relu_1():
+@pytest.mark.parametrize("backend", ["interpreter"])
+def test_relu_1(backend):
     with tempfile.TemporaryDirectory() as tmpdir:
-        op_elemwise(os.path.join(tmpdir, "model.onnx"), "Relu", [1, 2])
+        op_elemwise(os.path.join(tmpdir, "model.onnx"), "Relu", [1, 2], backend)
 
 
-def test_relu_2():
+@pytest.mark.parametrize("backend", ["interpreter"])
+def test_relu_2(backend):
     with tempfile.TemporaryDirectory() as tmpdir:
-        op_elemwise(os.path.join(tmpdir, "model.onnx"), "Relu", [3, 1, 28, 28])
+        op_elemwise(os.path.join(tmpdir, "model.onnx"), "Relu", [3, 1, 28, 28], backend)
 
 
-def test_hardsigmoid_1():
+@pytest.mark.parametrize("backend", ["interpreter"])
+def test_hardsigmoid_1(backend):
     with tempfile.TemporaryDirectory() as tmpdir:
-        op_elemwise(os.path.join(tmpdir, "model.onnx"), "HardSigmoid", [1, 2])
+        op_elemwise(os.path.join(tmpdir, "model.onnx"), "HardSigmoid", [1, 2], backend)
 
 
-def test_hardsigmoid_2():
+@pytest.mark.parametrize("backend", ["interpreter"])
+def test_hardsigmoid_2(backend):
     with tempfile.TemporaryDirectory() as tmpdir:
-        op_elemwise(os.path.join(tmpdir, "model.onnx"), "HardSigmoid", [3, 1, 28, 28])
+        op_elemwise(
+            os.path.join(tmpdir, "model.onnx"), "HardSigmoid", [3, 1, 28, 28], backend
+        )
 
 
-def test_leakyrelu_1():
+@pytest.mark.parametrize("backend", ["interpreter"])
+def test_leakyrelu_1(backend):
     with tempfile.TemporaryDirectory() as tmpdir:
-        op_elemwise(os.path.join(tmpdir, "model.onnx"), "LeakyRelu", [1, 2])
+        op_elemwise(os.path.join(tmpdir, "model.onnx"), "LeakyRelu", [1, 2], backend)
 
 
-def test_leakyrelu_2():
+@pytest.mark.parametrize("backend", ["interpreter"])
+def test_leakyrelu_2(backend):
     with tempfile.TemporaryDirectory() as tmpdir:
-        op_elemwise(os.path.join(tmpdir, "model.onnx"), "LeakyRelu", [3, 1, 28, 28])
+        op_elemwise(
+            os.path.join(tmpdir, "model.onnx"), "LeakyRelu", [3, 1, 28, 28], backend
+        )
 
 
-def test_sigmoid_1():
+@pytest.mark.parametrize("backend", ["interpreter"])
+def test_sigmoid_1(backend):
     with tempfile.TemporaryDirectory() as tmpdir:
-        op_elemwise(os.path.join(tmpdir, "model.onnx"), "Sigmoid", [1, 2])
+        op_elemwise(os.path.join(tmpdir, "model.onnx"), "Sigmoid", [1, 2], backend)
 
 
-def test_sigmoid_2():
+@pytest.mark.parametrize("backend", ["interpreter"])
+def test_sigmoid_2(backend):
     with tempfile.TemporaryDirectory() as tmpdir:
-        op_elemwise(os.path.join(tmpdir, "model.onnx"), "Sigmoid", [3, 1, 28, 28])
+        op_elemwise(
+            os.path.join(tmpdir, "model.onnx"), "Sigmoid", [3, 1, 28, 28], backend
+        )
 
 
-def test_sigmoid_3():
+@pytest.mark.parametrize("backend", ["interpreter"])
+def test_sigmoid_3(backend):
     with tempfile.TemporaryDirectory() as tmpdir:
-        op_elemwise(os.path.join(tmpdir, "model.onnx"), "Sigmoid", [3, 1, 7, 7])
+        op_elemwise(
+            os.path.join(tmpdir, "model.onnx"), "Sigmoid", [3, 1, 7, 7], backend
+        )
 
 
 @pytest.mark.xfail
-def test_clip_1():
+@pytest.mark.parametrize("backend", ["interpreter"])
+def test_clip_1(backend):
     with tempfile.TemporaryDirectory() as tmpdir:
-        op_elemwise(os.path.join(tmpdir, "model.onnx"), "Clip", [1, 2])
+        op_elemwise(os.path.join(tmpdir, "model.onnx"), "Clip", [1, 2], backend)
 
 
 @pytest.mark.xfail
-def test_clip_2():
+@pytest.mark.parametrize("backend", ["interpreter"])
+def test_clip_2(backend):
     with tempfile.TemporaryDirectory() as tmpdir:
-        op_elemwise(os.path.join(tmpdir, "model.onnx"), "Clip", [3, 1, 28, 28])
+        op_elemwise(os.path.join(tmpdir, "model.onnx"), "Clip", [3, 1, 28, 28], backend)
 
 
-def test_sqrt_1():
+@pytest.mark.parametrize("backend", ["interpreter", "cpu"])
+def test_sqrt_1(backend):
     with tempfile.TemporaryDirectory() as tmpdir:
-        op_elemwise(os.path.join(tmpdir, "model.onnx"), "Sqrt", [1, 2])
+        op_elemwise(os.path.join(tmpdir, "model.onnx"), "Sqrt", [1, 2], backend)
 
 
-def test_sqrt_2():
+@pytest.mark.parametrize("backend", ["interpreter", "cpu"])
+def test_sqrt_2(backend):
     with tempfile.TemporaryDirectory() as tmpdir:
-        op_elemwise(os.path.join(tmpdir, "model.onnx"), "Sqrt", [3, 1, 28, 28])
+        op_elemwise(os.path.join(tmpdir, "model.onnx"), "Sqrt", [3, 1, 28, 28], backend)
 
 
-def test_softmax_1():
+@pytest.mark.parametrize("backend", ["interpreter"])
+def test_softmax_1(backend):
+    with tempfile.TemporaryDirectory() as tmpdir:
+        op_elemwise(os.path.join(tmpdir, "model.onnx"), "Softmax", [1, 2, 3], backend)
+
+
+@pytest.mark.parametrize("backend", ["interpreter"])
+def test_softmax_2(backend):
+    with tempfile.TemporaryDirectory() as tmpdir:
+        op_elemwise(os.path.join(tmpdir, "model.onnx"), "Softmax", [3, 28, 28], backend)
+
+
+@pytest.mark.parametrize("backend", ["interpreter"])
+def test_softmax_3(backend):
+    with tempfile.TemporaryDirectory() as tmpdir:
+        op_elemwise(
+            os.path.join(tmpdir, "model.onnx"), "Softmax", [128, 512, 512], backend
+        )
+
+
+@pytest.mark.parametrize("backend", ["interpreter"])
+def test_erf_1(backend):
     with tempfile.TemporaryDirectory() as tmpdir:
         op_elemwise(
             os.path.join(tmpdir, "model.onnx"),
-            "Softmax",
+            "Erf",
             [1, 2, 3],
+            backend,
+            atol=1e-1,
+            rtol=1e-5,
         )
 
 
-def test_softmax_2():
+@pytest.mark.parametrize("backend", ["interpreter"])
+def test_erf_2(backend):
     with tempfile.TemporaryDirectory() as tmpdir:
         op_elemwise(
             os.path.join(tmpdir, "model.onnx"),
-            "Softmax",
+            "Erf",
             [3, 28, 28],
-        )
-
-
-def test_softmax_3():
-    with tempfile.TemporaryDirectory() as tmpdir:
-        op_elemwise(
-            os.path.join(tmpdir, "model.onnx"),
-            "Softmax",
-            [128, 512, 512],
-        )
-
-
-def test_erf_1():
-    with tempfile.TemporaryDirectory() as tmpdir:
-        op_elemwise(
-            os.path.join(tmpdir, "model.onnx"), "Erf", [1, 2, 3], atol=1e-1, rtol=1e-5
-        )
-
-
-def test_erf_2():
-    with tempfile.TemporaryDirectory() as tmpdir:
-        op_elemwise(
-            os.path.join(tmpdir, "model.onnx"), "Erf", [3, 28, 28], atol=1e-1, rtol=1e-5
+            backend,
+            atol=1e-1,
+            rtol=1e-5,
         )
 
 
@@ -158,7 +185,7 @@ def test_gelu_1():
             assert np.allclose(expected, actual, atol=1e-1, rtol=1e-5)
 
 
-def op_elemwise(filepath, op_type, shape, **kwargs):
+def op_elemwise(filepath, op_type, shape, backend, **kwargs):
     inputs = [helper.make_tensor_value_info("x", TensorProto.FLOAT, shape)]
     outputs = [helper.make_tensor_value_info("y", TensorProto.FLOAT, shape)]
     nodes = [helper.make_node(op_type, ["x"], ["y"])]
@@ -167,7 +194,7 @@ def op_elemwise(filepath, op_type, shape, **kwargs):
 
     onnx.save(model, filepath)
     ort_sess = ort.InferenceSession(filepath, providers=["CPUExecutionProvider"])
-    altius_sess = altius_py.InferenceSession(filepath)
+    altius_sess = altius_py.InferenceSession(filepath, backend=backend)
 
     x = np.random.random_sample(shape).astype(np.float32)
     expected = ort_sess.run(None, {"x": x})
